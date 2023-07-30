49323f8fcd97a1a0c4bd65ef4cb92e9756d2e771
Felipe Hidequel <felipehidequel@gmail.com>
2023-07-30 01:48:47 -0300

    fruta: modularização

--- fruta/fruta.c
+++ fruta/fruta.c
@@ -5,14 +5,11 @@ Cadastrar uma nova fruta: Solicitar ao usuário que insira o nome, cor, sabor e 
 Listar todas as frutas cadastradas: Imprimir na tela todas as informações das frutas cadastradas até o momento.
 Buscar fruta por nome: Solicitar ao usuário que insira o nome de uma fruta. O programa deve buscar no array de structs por todas as frutas com esse nome e imprimir suas informações.
 */
+#include "fruta.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_FRUTAS 2
 
-typedef struct fruta Fruta;
-
-/*Alocando memoria para Array structs do tipo Fruta*/
 Fruta **allocFruta()
 {
     Fruta **f = (Fruta **)malloc(MAX_FRUTAS * sizeof(Fruta *));
@@ -24,86 +21,12 @@ Fruta **allocFruta()
     return f;
 }
 
-/*
-    Cadastrar uma nova fruta:
-    Solicitar ao usuário que insira o nome, cor, sabor e quantidade da fruta.
-    Em seguida, adicionar a fruta ao array de structs.
-*/
-Fruta *cadastra(char *nome);
-/*
-    Listar todas as frutas cadastradas: Imprimir na tela todas as informações das frutas cadastradas até o momento.
-*/
-void listar(Fruta **frutas, int nFrutas);
-/*
-    Buscar fruta por nome:
-    Função busca no array de structs por todas as frutas com esse nome e imprimir suas informações.
-*/
-Fruta *buscar(char *nome, int nFrutas);
-
-int main(void)
-{
-    Fruta **frutas = allocFruta();
-    char nome[21];
-    int escolha = 0, nFrutas = 0;
-
-    while (escolha != 3) // se o usuario digite 3, o programa é encerrado
-    {
-        printf("\nMenu:\n 0 - Cadastrar nova fruta\n 1 - Listar frutas\n 2 - Buscar fruta\n 3 - Sair\n");
-        scanf("%d", &escolha);
-
-        switch (escolha)
-        {
-        case 0:
-            printf("Cadastrando...\n");
-            printf("Digite o nome da fruta: ");
-            scanf(" %[^\n]", nome);
-            if (nFrutas == MAX_FRUTAS)
-            {
-                printf("Número maximo de frutas cadastradas atingido!\n");
-            }
-            else
-            {
-                frutas[nFrutas] = cadastra(nome);
-                nFrutas++;
-            }
-
-            break;
-        case 1:
-            if (nFrutas == 0)
-            {
-                printf("Nenhuma fruta cadastrada\n");
-            }
-            else
-            {
-                listar(frutas, nFrutas);
-            }
-            break;
-        default:
-            break;
-        }
-    }
-
-    return 0;
-}
-
-/*
-    Contém caractericas de uma determinada fruta.
-    Campos: nome (string), cor (string), sabor (string) e quantidade (int)
-*/
-struct fruta
-{
-    char nome[21];
-    char cor[21];
-    char sabor[21];
-    int quantidade;
-};
-
 Fruta *cadastra(char *nome)
 {
     Fruta *f = (Fruta *)malloc(sizeof(Fruta));
     if (f == NULL)
     {
-        printf("Erro ao alocar memoria para frutas");
+        printf("Erro ao alocar memoria para f");
         exit(1);
     }
     strcpy(f->nome, nome);
@@ -123,6 +46,19 @@ void listar(Fruta **frutas, int nFrutas)
     for (int fruta = 0; fruta < nFrutas; fruta++)
     {
         Fruta *f = frutas[fruta];
-        printf("Nome: %s\nCor: %s\nSabor: %s\nQuantidade: %d\n", f->nome, f->cor, f->sabor, f->quantidade);
+        printf("Nome: %s\nCor: %s\nSabor: %s\nQuantidade: %d\n\n", f->nome, f->cor, f->sabor, f->quantidade);
     }
+}
+
+Fruta *buscar(Fruta **frutas, char *nome, int nFrutas)
+{
+    for (int fruta = 0; fruta < nFrutas; fruta++)
+    {
+        if (strcmp(nome, frutas[fruta]->nome) == 0)
+        {
+            return frutas[fruta];
+        }
+    }
+
+    return NULL;
 }
